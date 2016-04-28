a671f78020c33e0c74c869f019ad7ec90d621417
Marc Williams <oti_instructor@hotmail.com>
2016-04-28 14:22:27 -0300

    Added department pictures

--- ContosoUDemo/Controllers/HomeController.cs
+++ ContosoUDemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContosoUDemo.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,15 @@ namespace ContosoUDemo.Controllers
 {
     public class HomeController : Controller
     {
+        //mwilliams:  for departments
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
-            return View();
+            //return View();
+            var departments = db.Departments
+                .Where(d=>d.Name!="Temp")
+                .OrderBy(d => d.Name).ToList();
+            return View(departments);
         }
 
         public ActionResult About()
---
+++ ContosoUDemo/Controllers/PeopleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUDemo.DAL;
+using ContosoUDemo.Models;
+
+namespace ContosoUDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
